a80a56ffee662d46b0007f24ed1f3877b2f07ff7
dcardenas11 <danicardenas11@gmail.com>
2022-12-24 19:14:28 +0100

    trabajo de logistica

--- Ghandi/logistica/views.py
+++ Ghandi/logistica/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+
+
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, World!")
+
+def menu_logistica(request):
+    return render(request, "menu_logistica.html")
+
+def hacer_pedido(request):
+    return HttpResponse("Hello, World!")
+
+def consultar_stock(request):
+    return HttpResponse("Hello, World!")
+
+def almacenar_producto(request):
+    return HttpResponse("Hello, World!")
+
+def asignar_jaula(request):
+    return HttpResponse("Hello, World!")
+
+def consultar_disponibilidad_jaulas(request):
+    return HttpResponse("Hello, World!")
+
+def vaciar_jaula(request):
+    return HttpResponse("Hello, World!")
+
+def alta_producto_almacen(request):
+    return HttpResponse("Hello, World!")
+
+def menu_general(request):
+    return HttpResponseRedirect("login")
