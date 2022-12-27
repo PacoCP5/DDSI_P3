c3b03228a5a98f28b6d100905080f682899024f4
PacoCP5 <pacocamaraparra5@gmail.com>
2022-12-27 21:11:48 +0100

    Subsistema de Contabilidad acabado salvo repasar para ver fallos

--- Ghandi/contabilidad/forms.py
+++ Ghandi/contabilidad/forms.py
@@ -3,3 +3,13 @@ from django import forms
 class IngresoSalariosForm(forms.Form):
     dni = forms.CharField(label="DNI",widget=forms.TextInput(attrs={'placeholder': "Introduce DNI del trabajador"}))
     
+class FacturaForm(forms.Form):
+    idfactura = forms.CharField(label="idFactura",widget=forms.TextInput(attrs={'placeholder': "Introduce ID de la factura"}))
+    dni = forms.CharField(label="DNI",widget=forms.TextInput(attrs={'placeholder': "Introduce DNI del cliente"}))
+    cantidad = forms.CharField(label="Cantidad",widget=forms.TextInput(attrs={'placeholder': "Introduce cantidad"}))
+
+class PagoFacturaForm(forms.Form):
+    idfactura = forms.CharField(label="idFactura",widget=forms.TextInput(attrs={'placeholder': "Introduce ID de la factura"}))
+
+class PagoPedidoForm(forms.Form):
+    idpedido = forms.CharField(label="idPedido",widget=forms.TextInput(attrs={'placeholder': "Introduce ID del pedido"}))
